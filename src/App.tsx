fcc04c4561d8e37e136fa34040e73c37647234ac
DESKTOP-9HSRLI7\admin <tejaswibhokare17@gmail.com>
2024-01-02 14:56:19 -0500

    Added initial setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/navbar';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/auth" element={<h1>Auth</h1>} />
+          <Route path="/rated" element={<h1>Rated Page</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
